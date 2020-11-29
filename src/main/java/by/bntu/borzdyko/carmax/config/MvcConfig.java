93e675989dcef868572c403e7ad01f4b1346c91f
Maxim Borzdyko <maxim.borzdyko@gmail.com>
2020-11-29 15:52:35 +0300

    MvcConfig added

---
+++ src/main/java/by/bntu/borzdyko/carmax/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package by.bntu.borzdyko.carmax.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("${app.upload.dir:${user.home}}" + "\\CarmaxPictures\\Saved")
+    private String UPLOAD_PATH;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:" + UPLOAD_PATH + File.separator);
+    }
+}
